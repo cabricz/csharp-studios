ef6ef5964a47608fee20ba30616a28776e7ce777
Zak Cabric <cabricz@gmail.com>
2018-02-19 01:25:42 -0500

    finish studio 1

---
+++ AreaOfACircle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double area;
+
+            do
+            {
+                Console.WriteLine("Enter a radius:");
+                r = double.Parse(Console.ReadLine());
+                if (r < 0)
+                {
+                    Console.WriteLine("Please enter a positive number!");
+                }
+            } while (r < 0);
+
+            area = 3.14 * r * r;
+            Console.WriteLine("The area of a circle with radius " + r + " is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
+© 2018 GitHub, Inc.
